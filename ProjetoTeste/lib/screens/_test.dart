80c979d7ad34f04fede1f58d27618746141a5a2f
Lucas Walker <jose.batista09322@alunos.ufersa.edu.br>
2023-10-19 19:06:03 -0300

    modificando o arquivo de testes

--- ProjetoTeste/lib/screens/_test.dart
+++ ProjetoTeste/lib/screens/_test.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:appbd/classes/movie.dart';
+import 'create_acount.dart';
 
 void main() {
   test('Teste de Construção da Classe Movie', () {
@@ -21,4 +22,27 @@ void main() {
     expect(movie.releaseDate, DateTime(2023, 10, 1));
     expect(movie.reviewId, 123);
   });
+
+  // testWidgets('Teste de Funcionalidade CreateAcoount',
+  //     (WidgetTester tester) async {
+  //   await tester.pumpWidget(MaterialApp(
+  //     home: const CreateAcoount(),
+  //   ));
+
+  //   final nameTextField = find.byType(TextField).at(0);
+  //   final emailTextField = find.byType(TextField).at(1);
+  //   final passwordTextField = find.byType(TextField).at(2);
+
+
+  //   await tester.enterText(nameTextField, 'Nome de Teste');
+  //   await tester.enterText(emailTextField, 'teste@email.com');
+  //   await tester.enterText(passwordTextField, 'senha123');
+
+
+  //   final createAccountButton = find.text('Criar conta');
+
+
+  //   await tester.tap(createAccountButton);
+  //   await tester.pump();
+  // });
 }
