8a4a7b70ef8b49867a794bc3a9227ab677b4e530
Lucas Walker <jose.batista09322@alunos.ufersa.edu.br>
2023-10-19 19:02:27 -0300

    testes unitarios, funcionabilidade, widgets

--- ProjetoTeste/lib/screens/_test.dart
+++ ProjetoTeste/lib/screens/_test.dart
@@ -1,18 +1,24 @@
-import "package:flutter_test/flutter_test.dart";
-import 'until.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:appbd/classes/movie.dart';
 
 void main() {
-  test('Teste de e-mail válido', () {
-    expect(isValidEmail('email@example.com'), isTrue);
-  });
+  test('Teste de Construção da Classe Movie', () {
+    final movie = Movie(
+      id: 1,
+      image: 'image_path',
+      genre: 'Ação',
+      name: 'Nome do Filme',
+      rating: 4.5,
+      releaseDate: DateTime(2023, 10, 1),
+      reviewId: 123,
+    );
 
-  test('Teste de e-mail inválido (vazio)', () {
-    expect(isValidEmail(''), isFalse);
+    expect(movie.id, 1);
+    expect(movie.image, 'image_path');
+    expect(movie.genre, 'Ação');
+    expect(movie.name, 'Nome do Filme');
+    expect(movie.rating, 4.5);
+    expect(movie.releaseDate, DateTime(2023, 10, 1));
+    expect(movie.reviewId, 123);
   });
-
-  test('Teste de e-mail inválido (formato incorreto)', () {
-    expect(isValidEmail('email@exemplo'), isFalse);
-  });
-
-  // Adicione mais testes conforme necessário
 }
